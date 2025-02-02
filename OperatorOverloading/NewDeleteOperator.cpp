2fdb7840cb803e1a3088c378c8452e59875c5702
Shreekrishna Hegde <hegdeshreekrishna469@gmail.com>
2025-02-02 22:12:29 +0530

    Minor Changes

---
+++ OperatorOverloading/NewDeleteOperator.cpp
@@ -0,0 +1,23 @@
+/*
+void operator delete(void * pointer)
+{
+    // freeing the memory pointed to by the pointer pointer
+    // ...
+}
+
+return_type * operator new(size_t size)
+{
+    // Memory allocation
+    // ...
+}
+
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    return 0;
+}
