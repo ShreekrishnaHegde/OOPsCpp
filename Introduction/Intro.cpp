df88aed7c7bee1ab6880b7e2341b9901ef7a2f32
Shreekrishna Hegde <hegdeshreekrishna469@gmail.com>
2025-02-20 00:01:26 +0530

    Initial Commit

---
+++ Introduction/Intro.cpp
@@ -0,0 +1,27 @@
+/*
+
+| C                                             | C++                                      |
+|-----------------------------------------------|---------------------------------|
+| Supports procedural programming.              | Supports both procedural and object-oriented programming. |
+| Data and functions are separated (procedural).| Data and functions are encapsulated in objects (OOP). |
+| Does not support data hiding.                 | Data is hidden by encapsulation.          |
+| Function-driven (procedural).                 | Object-driven (OOP).                      |
+| Function &operator overloading not supported. | Function and operator overloading supported. |
+| Functions not defined inside structures.      | Functions can be inside structures.       |
+| No namespaces.                                | Uses namespaces to avoid name collisions. |
+| Does not support reference variables.         | Supports reference variables.             |
+| No virtual and friend functions.              | Supports virtual and friend functions.    |
+| Does not support inheritance.                 | Supports inheritance.                    |
+| No direct support for exception handling.     | Supports exception handling.              |
+| No access modifiers in structures.            | Structures have access modifiers.         |
+| No strict type checking.                      | Strict type checking. Programs that run in C may show warnings/errors in C++. |
+| File extension is “.c”                        | File extension is “.cpp”, “.c++”, “.cc”, “.cxx” |
+| Does not support polymorphism,                | Supports polymorphism, encapsulation, and inheritance (OOP language). |
+|   encapsulation, and inheritance              |
+| Uses malloc() and calloc() for memory         | Uses new and delete operators for memory management. |
+   allocation, free() for de-allocation.        | 
+
+
+
+
+*/
