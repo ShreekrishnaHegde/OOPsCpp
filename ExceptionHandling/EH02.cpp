14eedadc0b1dea2aa02b119c5c1c33151ca05132
Shreekrishna Hegde <hegdeshreekrishna469@gmail.com>
2025-02-19 10:05:41 +0530

    Initial Commit

--- ExceptionHandling/EH02.cpp
+++ ExceptionHandling/EH02.cpp
@@ -1,22 +1,25 @@
+// program to divide two numbers
+// throws an exception when the divisor is 0
+
 #include <iostream>
 using namespace std;
-
-// function to throw division by zero exception
-void divide(int num, int den) {
-    if (den == 0) {
-        throw runtime_error("Division by zero error");
-    }
-    cout << "Result of division: " << num / den << endl;
-}
-
 int main() {
-    int numerator = 5, denominator = 0;
+    double numerator, denominator, divide;
+    cout << "Enter numerator: ";
+    cin >> numerator;
+    cout << "Enter denominator: ";
+    cin >> denominator;
 
     try {
-        divide(numerator, denominator);
-    } catch (const exception& e) {
-        cout << "Error: " << e.what() << endl;
+        // throw an exception if denominator is 0
+        if (denominator == 0)
+            throw 0;
+        // not executed if denominator is 0
+        divide = numerator / denominator;
+        cout << numerator << " / " << denominator << " = " << divide << endl;
+    } 
+    catch (int num_exception) {
+        cout << "Error: Cannot divide by " << num_exception << endl;
     }
-
     return 0;
 }
